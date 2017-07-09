a61b8a8a6b2d3d47a79fec981b095ae4c6f06dab
Phil Porada <philporada@gmail.com>
2017-07-09 19:53:14 -0400

    Fixed rsync

--- ci-scripts/prep-and-rsync.sh
+++ ci-scripts/prep-and-rsync.sh
@@ -3,5 +3,5 @@ set -e
 set -x
 rm -rf .git* Jenkinsfile .well-known
 DIR="/var/www/domains/philporada.com/www/htdocs/"
-rsync -avHsP --delete --exclude .well-known site/ "${DIR}"
+rsync -avHSP --omit-dir-times --delete --exclude .well-known site/ "${DIR}"
 restorecon "${DIR}"

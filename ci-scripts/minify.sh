5b474a13fc6285f1cad0bc6d4fcfa9f769b81a42
Phil Porada <philporada@gmail.com>
2017-07-09 19:39:36 -0400

    Please work

--- ci-scripts/minify.sh
+++ ci-scripts/minify.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
 set -x
-find . -type f -name "*.html" -o -name "*.js" -o -name "*.css" -o -name "*.json" -o -name "*.svg" -o -name "*.xml" -exec minify {} -o {} \;
+find . -type f \
+    -name "*.html" \
+    -o -name "*.js" \
+    -o -name "*.css" \
+    -o -name "*.json" \
+    -o -name "*.svg" \
+    -o -name "*.xml" \
+    -exec "minify {} -o {}" \;
